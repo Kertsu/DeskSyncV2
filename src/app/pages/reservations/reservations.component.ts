88c91cc4dfa809b67dd9296e42359bc7f4980760
K3rtsu <kurtddaniel@gmail.com>
2024-05-08 17:14:03 +0800

    add table on reservation screen

--- src/app/pages/reservations/reservations.component.ts
+++ src/app/pages/reservations/reservations.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Reservation } from '../../models/Reservation';
+import { MessageService, ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-reservations',
   templateUrl: './reservations.component.html',
-  styleUrl: './reservations.component.css'
+  styleUrl: './reservations.component.css',
+  providers: [MessageService, ConfirmationService],
 })
 export class ReservationsComponent {
+  loading: boolean = false;
+  totalRecords!: number;
+  selectAll: boolean = false;
 
+  reservations: Reservation[] = [];
+  selectedReservations!: any[] | null;
+
+  cancelSelectedReservations() {}
+  loadReservations(event: any) {}
+  onSelectionChange(event: any) {}
+  onSelectAllChange(event: any) {}
+
+  getImage(reservation: Reservation) {}
+  confirm(reservation: Reservation) {}
 }
