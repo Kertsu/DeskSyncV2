4432ce7adac3b2f97dabe84bcb1f1532ba4b0e71
Kertsu <kurtddbigtas@gmail.com>
2024-05-30 20:20:27 +0800

    updating toRate value

--- src/app/components/to-rate/to-rate.component.ts
+++ src/app/components/to-rate/to-rate.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WebService } from '../../services/web.service';
 import { ParamsBuilderService } from '../../services/params-builder.service';
 import { SocketService } from '../../services/socket.service';
@@ -8,6 +8,7 @@ import {
   DynamicDialogRef,
 } from 'primeng/dynamicdialog';
 import { FeedbackComponent } from '../feedback/feedback.component';
+import { UserService } from '../../services/user.service';
 
 interface ToRate {
   user: string;
@@ -23,7 +24,7 @@ interface ToRate {
   styleUrl: './to-rate.component.css',
   providers: [DialogService],
 })
-export class ToRateComponent implements OnInit {
+export class ToRateComponent implements OnInit, OnDestroy {
   toRateReservations: ToRate[] = [];
 
   ref: DynamicDialogRef | undefined = undefined;
@@ -32,14 +33,27 @@ export class ToRateComponent implements OnInit {
   limit: number = 10;
   totalRecords!: number;
 
+  user!: any;
+
+  storageEventListener!: (event: StorageEvent) => void
+
   constructor(
     private webService: WebService,
     private paramsBuilder: ParamsBuilderService,
     private socketService: SocketService,
-    private dialogService: DialogService
-  ) {}
+    private dialogService: DialogService,
+    private userService: UserService
+  ) {
+    this.storageEventListener = (event: StorageEvent) => {
+      if (event.key === 'hdbsv2User') {
+        this.user = JSON.parse(event.newValue as string);
+      }
+    };
+    window.addEventListener('storage', this.storageEventListener);
+  }
 
   ngOnInit(): void {
+    this.user = this.userService.getUser();
     const eventParams = {
       mode: 0,
       filters: {
@@ -71,7 +85,17 @@ export class ToRateComponent implements OnInit {
     this.socketService.reservationEnded.subscribe((res: any) => {
       this.toRateReservations = this.toRateReservations.concat(res);
       this.totalRecords = this.toRateReservations.length;
+      if (this.user) {
+        this.user.toRate += 1;
+      }
+      this.userService.setUser(this.user);
     });
+
+    window.addEventListener('storage', this.storageEventListener, false);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('storage', this.storageEventListener);
   }
   getImage(deskNumber: number) {
     return `../../assets/images/map/desk-area/${deskNumber}.png`;
@@ -119,10 +143,13 @@ export class ToRateComponent implements OnInit {
     };
     this.ref = this.dialogService.open(FeedbackComponent, dialogConfig);
 
+    let self = this;
     this.ref.onClose.subscribe((feedback) => {
-      this.toRateReservations = this.toRateReservations.filter(
-        (r) => r.reservation !== feedback.reservation
-      );
+      this.toRateReservations = this.toRateReservations.filter((r) => {
+        self.user.toRate -= 1;
+        self.userService.setUser(self.user)
+        return r.reservation !== feedback.reservation;
+      });
     });
   }
 }
